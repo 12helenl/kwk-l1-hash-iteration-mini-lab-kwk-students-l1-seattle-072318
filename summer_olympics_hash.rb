e5c2ce23956e3cc8ad70478dbb331d70804dad7b
12helenl <12helenl@gmail.com>
2018-07-26 16:15:21 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -12,7 +12,7 @@
   
   create_olympics_hash[:atlanta] = "1996"
   
-  
+  puts create_olympics_hash.upcase
 
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
@@ -24,6 +24,6 @@ end
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
 
-create_olympics_hash.upcase
+
 
 
