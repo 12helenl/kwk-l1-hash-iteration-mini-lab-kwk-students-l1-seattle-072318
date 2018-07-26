aa18967b2decb52f06022e0d457e9c7f3eb5b092
12helenl <12helenl@gmail.com>
2018-07-26 16:14:56 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -24,6 +24,6 @@ end
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
 
-puts create_olympics_hash.upcase
+create_olympics_hash.upcase
 
 
